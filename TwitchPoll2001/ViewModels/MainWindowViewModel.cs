dc0280fcf2f03cd8a3e3dbceaca79b01ef016c7a
Tim Miller <innerlogic4321@gmail.com>
2017-03-02 11:37:10 -0500

    Add initial commit

---
+++ TwitchPoll2001/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib;
+using TwitchLib.Events.Client;
+using TwitchLib.Models.Client;
+
+namespace TwitchPoll2001.ViewModels
+{
+    public class MainWindowViewModel : NotifierBase
+    {
+        public MainWindowViewModel()
+        {
+            ClickLoginButtonCommand = new AsyncDelegateCommand(async o => { await ClickLoginButton(); },
+                o => CanClickLoginButton);
+
+            JoinChannelCommand = new AsyncDelegateCommand(async o => { await JoinChannel(); },
+                o => CanJoinChannel);
+
+            LeaveChannelCommand = new AsyncDelegateCommand(async o => { await LeaveChannel(); },
+                o => true);
+        }
+
+        #region Properties
+        public bool CanClickLoginButton => !string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password);
+
+        public bool CanJoinChannel => IsLoggedIn && !string.IsNullOrEmpty(ChannelName);
+
+        public bool CanLeaveChannel => IsLoggedIn;
+
+
+        public AsyncDelegateCommand ClickLoginButtonCommand { get; private set; }
+
+        public AsyncDelegateCommand JoinChannelCommand { get; private set; }
+
+        public AsyncDelegateCommand LeaveChannelCommand { get; private set; }
+
+        public TwitchClient Client { get; set; }
+
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set
+            {
+                SetProperty(ref _isLoading, value);
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isLoggedIn;
+        public bool IsLoggedIn
+        {
+            get { return _isLoggedIn; }
+            set
+            {
+                SetProperty(ref _isLoggedIn, value);
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _connectedToChannel;
+        public bool ConnectedToChannel
+        {
+            get { return _connectedToChannel; }
+            set
+            {
+                SetProperty(ref _connectedToChannel, value);
+                OnPropertyChanged();
+            }
+        }
+
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                if (_userName == value) return;
+                _userName = value;
+                OnPropertyChanged();
+                ClickLoginButtonCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (_password == value) return;
+                _password = value;
+                OnPropertyChanged();
+                ClickLoginButtonCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _channelName;
+
+        public string ChannelName
+        {
+            get { return _channelName; }
+            set
+            {
+                if (_channelName == value) return;
+                _channelName = value;
+                OnPropertyChanged();
+                JoinChannelCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        #endregion
+
+        #region TwitchEvents
+        private void ClientConnected(object sender, OnConnectedArgs e)
+        {
+            Console.WriteLine("Client connected to Twitch!");
+            IsLoggedIn = true;
+        }
+
+
+        private void ClientJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            Console.WriteLine($"Client joined channel: {e.Channel}");
+            ConnectedToChannel = true;
+            
+        }
+
+        private void ClientLeftChannel(object sender, OnLeftChannelArgs e)
+        {
+            Console.WriteLine($"Client left channel: {e.Channel}");
+            ConnectedToChannel = false;
+        }
+
+        private void ClientWhisperReceived(object sender, OnWhisperReceivedArgs e)
+        {
+            Console.WriteLine($"Client whisper received: {e.WhisperMessage}");
+        }
+
+        private void ClientWhisperCommandReceived(object sender, OnWhisperCommandReceivedArgs e)
+        {
+            Console.WriteLine($"Client whisper command: {e.Command} - {e.WhisperMessage.Message}");
+        }
+
+        private void ClientMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            Console.WriteLine($"Client message: {e.ChatMessage.Message}");
+        }
+
+        private void ClientChatCommandReceived(object sender, OnChatCommandReceivedArgs e)
+        {
+            Console.WriteLine($"Client whisper command: {e.Command}");
+            // ChatCommand
+        }
+
+        #endregion
+
+        #region TwitchCommands
+
+        public async Task LeaveChannel()
+        {
+            if (Client == null) return;
+            Client.LeaveChannel(ChannelName);
+        }
+
+        public async Task JoinChannel()
+        {
+            if (Client == null) return;
+            Client.JoinChannel(ChannelName);
+        }
+
+        #endregion
+
+        public async Task ClickLoginButton()
+        {
+            try
+            {
+                Client = new TwitchClient(new ConnectionCredentials(UserName, Password), null, '!', '!', true);
+
+                Client.OnConnected += ClientConnected;
+                Client.OnJoinedChannel += ClientJoinedChannel;
+                Client.OnLeftChannel += ClientLeftChannel;
+                Client.OnMessageReceived += ClientMessageReceived;
+                Client.OnChatCommandReceived += ClientChatCommandReceived;
+                Client.OnWhisperCommandReceived += ClientWhisperCommandReceived;
+                Client.OnWhisperReceived += ClientWhisperReceived;
+                Client.Connect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+    }
+}
---
+++ TwitchPoll2001/ViewModels/NotifierBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TwitchPoll2001.ViewModels
+{
+    public abstract class NotifierBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void RaiseEvent<TArgs>(EventHandler<TArgs> handler, TArgs args)
+        {
+            handler?.Invoke(this, args);
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+
+    public sealed class AsyncDelegateCommand : ICommand
+    {
+        private readonly Func<object, Task> _asyncExecute;
+        private readonly Predicate<object> _canExecute;
+
+        public AsyncDelegateCommand(Func<object, Task> execute)
+            : this(execute, null)
+        {
+        }
+
+        public AsyncDelegateCommand(Func<object, Task> asyncExecute,
+            Predicate<object> canExecute)
+        {
+            _asyncExecute = asyncExecute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+
+            return _canExecute(parameter);
+        }
+
+        public async void Execute(object parameter)
+        {
+            await ExecuteAsync(parameter);
+        }
+
+        private async Task ExecuteAsync(object parameter)
+        {
+            await _asyncExecute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            EventHandler handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+
+}
